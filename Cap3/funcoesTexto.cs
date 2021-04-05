4c2e9b5bc1f07f5b44ec3858b50730b5d3171c87
Renato Haddad <rehaddad@msn.com>
2021-04-05 11:30:01 -0300

    setup inicial

---
+++ Cap3/funcoesTexto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace livrocsharp
+{
+    class funcoesTexto
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello texto!");
+        }
+    }
+}
