9f9610afc582c01c503db25a742c2697a52e2247
Ray Carneiro <ray@academiadotnet.com.br>
2021-05-29 16:56:12 -0300

    cap 2 - adicao de classes

---
+++ Cap2/Gerente.cs
@@ -0,0 +1,11 @@
+namespace SistemaBancario{
+    public class Gerente {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public int Cpf { get; set; }
+
+        public void AprovarHorasDeFuncionarios(int cpf){
+            //lógica para aprovar horas
+        }
+    }
+}
