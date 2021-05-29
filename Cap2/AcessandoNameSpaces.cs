d8415dc3ae05ecd1c012c9898615125f8a6fbe86
Ray Carneiro <ray@academiadotnet.com.br>
2021-05-29 15:36:11 -0300

    adicionando arquivos de namespace

---
+++ Cap2/AcessandoNameSpaces.cs
@@ -0,0 +1,12 @@
+using ProgramaPrincipal;
+
+namespace MyProgram
+{
+    public class ClasseUm
+    {
+        public static void Main()
+        {
+            ClasseDois ClasseDois = new ClasseDois();
+        }
+    }
+}
