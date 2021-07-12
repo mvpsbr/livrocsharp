ebc41286b0e61521769b20f0cd3bf9310344b9f5
Carlos dos Santos <cds@cds-software.com.br>
2021-07-11 22:52:27 -0300

    Cap 10 - inicial

---
+++ DownloadAssincrono/frmPrincipal.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace DownloadAssincrono
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.btnContar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPalavra = new System.Windows.Forms.TextBox();
+            this.lstStatus = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnLer
+            // 
+            this.btnLer.Location = new System.Drawing.Point(377, 6);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(75, 23);
+            this.btnLer.TabIndex = 3;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // txtSite
+            // 
+            this.txtSite.Location = new System.Drawing.Point(63, 6);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(308, 23);
+            this.txtSite.TabIndex = 2;
+            // 
+            // btnContar
+            // 
+            this.btnContar.Location = new System.Drawing.Point(244, 35);
+            this.btnContar.Name = "btnContar";
+            this.btnContar.Size = new System.Drawing.Size(75, 23);
+            this.btnContar.TabIndex = 5;
+            this.btnContar.Text = "Contar";
+            this.btnContar.UseVisualStyleBackColor = true;
+            this.btnContar.Click += new System.EventHandler(this.btnContar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Palavra";
+            // 
+            // txtPalavra
+            // 
+            this.txtPalavra.Location = new System.Drawing.Point(63, 35);
+            this.txtPalavra.Name = "txtPalavra";
+            this.txtPalavra.Size = new System.Drawing.Size(175, 23);
+            this.txtPalavra.TabIndex = 4;
+            // 
+            // lstStatus
+            // 
+            this.lstStatus.FormattingEnabled = true;
+            this.lstStatus.ItemHeight = 15;
+            this.lstStatus.Location = new System.Drawing.Point(12, 73);
+            this.lstStatus.Name = "lstStatus";
+            this.lstStatus.Size = new System.Drawing.Size(440, 304);
+            this.lstStatus.TabIndex = 6;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 394);
+            this.Controls.Add(this.lstStatus);
+            this.Controls.Add(this.txtPalavra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnContar);
+            this.Controls.Add(this.txtSite);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLer);
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exemplo de Download Assíncrono";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSite;
+        private System.Windows.Forms.Button btnContar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPalavra;
+        private System.Windows.Forms.ListBox lstStatus;
+    }
+}
+
---
+++ DownloadAssincrono/frmPrincipal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadAssincrono
+{
+    public partial class frmPrincipal : Form
+    {
+        string site;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+            this.btnLer.Click += async (o, e) => { await btnLer_Click(o, e); };
+            site = "";
+        }
+
+        private async Task btnLer_Click(object sender, EventArgs e)
+        {
+            lstStatus.Items.Add("Lendo site...");
+            lstStatus.Refresh();
+            var web = new HttpClient();
+            site = await web.GetStringAsync(new Uri(txtSite.Text));
+            if (site != "" && txtPalavra.Text != "")
+            {
+                lstStatus.Items.Add("Site lido");
+                btnContar_Click(sender, e);
+            }
+        }
+
+        private async void btnContar_Click(object sender, EventArgs e)
+        {
+            if (txtPalavra.Text != "")
+            {
+                var total = await ContarPalavras(txtPalavra.Text);
+                lstStatus.Items.Add($"Total de ocorrências da palavra {txtPalavra.Text} = { total}");
+            }
+            else
+            {
+                lstStatus.Items.Add("Palavra não informada");
+            }
+        }
+
+        private async Task<int> ContarPalavras(string palavra)
+        {
+            return await Task.Run(() => Regex.Matches(site, palavra).Count());
+        }
+    }
+}
