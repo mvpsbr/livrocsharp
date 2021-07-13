116b925436853f07f761124f584df8a4930a8fe0
Carlos dos Santos <cds@cds-software.com.br>
2021-07-12 22:35:03 -0300

    Cap 10 final

---
+++ Cap10/ExemploAPI/Controllers/DadosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExemploAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DadosController : ControllerBase
+    {
+        private readonly ILogger<DadosController> _logger;
+
+        public DadosController(ILogger<DadosController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sincrono")]
+        public string Get()
+        {
+            return "Exemplo de API - método sincrono";
+        }
+
+        [HttpGet("assincrono")]
+        public async Task<string> GetAsync()
+        {
+            var sr = new StreamReader("./arquivos/teste.txt");
+            var conteudo = await sr.ReadToEndAsync();
+            sr.Close();
+            return conteudo;
+            
+        }
+    }
+}
---
+++ Cap10/ExemploAPI/Startup.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ExemploAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
